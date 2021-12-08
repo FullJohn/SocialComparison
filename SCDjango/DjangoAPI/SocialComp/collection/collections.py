dde00eba385f335bb166a9783072ef910f9744fc
Pierce Hopkins <piercerod57@gmail.com>
2021-12-07 20:33:11 -0700

    uodated collections.py

--- SCDjango/DjangoAPI/SocialComp/collection/collections.py
+++ SCDjango/DjangoAPI/SocialComp/collection/collections.py
@@ -3,4 +3,11 @@ from .youtube.youtube_collect import run_youtube_collect
 def run_collection(platform, brands, date_range):
     if platform =='YouTube':
         run_youtube_collect(brands, date_range)
-
+    else if platform =='Twitter':
+        run_twitter_collect(brands, date_range)
+    else if platform =='Pinterest':
+        run_pinterest_collect(brands, date_range)
+    else if platform =='Facebook':
+        run_facebook_collect(brands, date_range)
+    else if platform =='Instagram':
+        run_instagram_collect(brands, date_range)
--- SCDjango/DjangoAPI/SocialComp/collection/pinterest/pinterest_collect.py
+++ SCDjango/DjangoAPI/SocialComp/collection/pinterest/pinterest_collect.py
@@ -1,18 +1,3 @@
-import pinterest_user
-import time
-import datetime
-
-
-firstDate =  datetime.datetime.now() - datetime.timedelta(30)
-lastDate  =  datetime.datetime.now()
-
-
-print('Beginning data retrieval...')
-oreo = pinterest_user.PinterestUser('oreo', firstDate, lastDate)
-
-oreo.retrieve_posts()
-oreo.parse_divs()
-
 import sys
 from . import pinterest_user
 from . import pinterest_post
